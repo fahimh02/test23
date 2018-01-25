d463ada2cf5014761454a3eda87d5917f5e1e2ee
Fahim Afser <fahimh02@gmail.com>
2018-01-25 22:54:44 +0600

    HelloWorld

---
+++ testSolutionGit/gitConsoleTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace gitConsoleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
